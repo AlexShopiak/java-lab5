b6d1e86306276bbb77852150280e9273d1cff004
Alex Shopiak <alshop2004@gmail.com>
2023-12-05 23:39:42 +0000

    Implement Punctuation class

---
+++ src/lexeme/Punctuation.java
@@ -0,0 +1,13 @@
+package lexeme;
+
+public class Punctuation {
+    private char data;
+
+    public Punctuation(char data) {
+        this.data = data;
+    }
+
+    public char get() {
+        return data;
+    }
+}
