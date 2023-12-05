eb291e3d3563eb9974c51ba568b7a65a6f336dbf
Alex Shopiak <alshop2004@gmail.com>
2023-12-05 23:38:22 +0000

    Implement Letter class

---
+++ src/lexeme/Letter.java
@@ -0,0 +1,13 @@
+package lexeme;
+
+public class Letter {
+    private char data;
+
+    public Letter(char data) {
+        this.data = data;
+    }
+
+    public char get() {
+        return data;
+    }
+}
