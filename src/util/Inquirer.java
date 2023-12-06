9c439c248181dfbf10c5af8ca7a3a44265c5de1b
Alex Shopiak <alshop2004@gmail.com>
2023-12-06 23:04:49 +0000

    Add bold font

--- src/util/Inquirer.java
+++ src/util/Inquirer.java
@@ -6,7 +6,7 @@ public class Inquirer {
     public static String inquire() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the text: ");
+        System.out.print("\u001B[1m" + "Enter the text: " + "\u001B[0m");
         String line = scanner.nextLine();
 
         scanner.close();
