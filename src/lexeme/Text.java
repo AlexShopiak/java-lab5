84239ee580184fdb227ccee3fdf3e2bd1f83596d
Alex Shopiak <alshop2004@gmail.com>
2023-12-06 23:12:40 +0000

    Reimplement Text class

--- src/lexeme/Text.java
+++ src/lexeme/Text.java
@@ -1,13 +1,50 @@
 package lexeme;
 
-public class Text{
-    private Sentence[] data;
+import java.util.ArrayList;
 
-    public Text(Sentence[] data) {
-        this.data = data;
+public class Text{
+    private ArrayList<Sentence> sentences;
+    private boolean isEmpty = true;
+
+    public Text(String input) {
+        sentences = new ArrayList<>();
+        if (input.length() > 0) {
+            parse(input);
+            isEmpty = false;
+        }
+    }
+    public Word create() {
+        return new Word("aboba");
     }
 
-    public Sentence[] get() {
-        return data;
+    public String get() {
+        if (isEmpty) {
+            return "";
+        } else {
+            return join();
+        }
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    private void parse(String input) {
+        String[] arr = input.split("(?<=[.!?])\\s");
+
+        for (String elem : arr) {
+            sentences.add(new Sentence(elem));
+        }
+    }
+
+    private String join() {
+        String output = "";
+
+        for (Sentence sent : sentences) {
+            output = output.concat(" ");
+            output = output.concat(sent.join());
+        }
+
+        return output.substring(1);
     }
 }
